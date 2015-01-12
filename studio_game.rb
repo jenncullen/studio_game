04a1b8c8e0bb99ab7f6676acfe92e40b14631838
jenncullen <jenncullen75@gmail.com>
2015-01-12 14:02:58 -0500

    Chapter 9 Arrays Section 2, Item #2

--- studio_game.rb
+++ studio_game.rb
@@ -1,10 +1,15 @@
 class Player
+  attr_reader :health
+  attr_accessor :name
+  def score
+    @health + @name.length
+  end
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
   end
   def to_s
-    "I'm #{@name} with a health of #{@health}."
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
   def blam
     @health -= 10
@@ -15,16 +20,16 @@ class Player
     "#{@name} got w00ted!"
   end
 end
-player1 = Player.new("moe")
-player2 = Player.new("larry",60)
-player3 = Player.new("curly", 125)
-puts player1
-puts player2
-puts player3
-puts player3.blam
-puts player3
-puts player3.w00t
-puts player3
+ 
+  player1 = Player.new("moe")
+  player2 = Player.new("larry",60)
+  player3 = Player.new("curly", 125)
+  players = [player1, player2, player3]
+
+  puts "There are #{players.size} players in the game:"
+
+
+
 
 
 
