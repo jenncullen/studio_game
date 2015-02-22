6f43ac6f3d692f8e0d0570ac6a2ae04b9598f6f9
jenncullen <jenncullen75@gmail.com>
2015-02-22 11:09:29 -0500

    Mixins Chapter Fixes

--- game_turn.rb
+++ game_turn.rb
@@ -4,6 +4,14 @@ module GameTurn
   
   def self.take_turn(player)
     die = Die.new
+    case die.roll
+    when 1..2
+      player.blam
+    when 3..4
+      puts "#{player.name} was skipped."
+    else
+      player.w00t
+    end
     treasure = TreasureTrove.random
     player.found_treasure(treasure)
   end
